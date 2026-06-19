6f03e4be6b14e0e147b0f23a82d25363d8aa45e2
Luis felipe tenorio <fiep91@hotmail.com>
2026-06-18 19:37:13 -0500

    Modal editable en Coordinador, eliminar edición inline, simplificar tabla

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -4,19 +4,11 @@ import { Navbar } from "../components/ui/Navbar";
 export const MainLayout = () => {
     return (
         <div className="min-h-screen flex flex-col">
-            {/* Fondo */}
-            <div className="fixed inset-0 -z-10">
-                <div className="
-                    h-full w-full
-                    bg-linear-to-br from-purple-900 via-purple-700 to-purple-500
-                    md:bg-cover md:bg-center md:bg-[url('/src/assets/img/fondo-tablet.png')]
-                    lg:bg-[url('/src/assets/img/fondo.png')]
-                " />
-            </div>
+            {/* Fondo fijo */}
+            <div className="fixed inset-0 -z-10 bg-gradient-to-br from-purple-900 via-purple-700 to-purple-500 md:bg-cover md:bg-center md:bg-[url('/src/assets/img/fondo-tablet.png')] lg:bg-cover lg:bg-center lg:bg-[url('/src/assets/img/fondo.png')]" />
 
             <Navbar />
 
-            
             <div className="flex-1 flex items-center justify-center p-4 pt-16 md:pt-20">
                 <div className="w-full max-w-7xl">
                     <Outlet />
@@ -24,4 +16,4 @@ export const MainLayout = () => {
             </div>
         </div>
     );
-}
+};
