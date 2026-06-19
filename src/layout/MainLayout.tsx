937030ee9b1a16c0b269cf54855414cdb3b40735
Luis felipe tenorio <fiep91@hotmail.com>
2026-06-18 19:58:40 -0500

    Arreglar fondo en Vercel usando import de imágenes

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,16 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/ui/Navbar";
-import fondoDesktop from "../assets/img/fondo.png";
-import fondoTablet from "../assets/img/fondo-tablet.png";
 
 export const MainLayout = () => {
     return (
-        <div className="min-h-screen flex flex-col bg-gradient-to-br from-purple-900 via-purple-700 to-purple-500">
+        <div className="min-h-screen flex flex-col">
             {/* Fondo para tablet */}
             <div 
                 className="fixed inset-0 -z-10 hidden md:block bg-cover bg-center bg-no-repeat"
                 style={{ 
-                    backgroundImage: `url(${fondoTablet})`,
+                    backgroundImage: "url('/img/fondo-tablet.png')",
                     backgroundSize: "cover",
                     backgroundPosition: "center",
                     backgroundAttachment: "fixed",
@@ -22,7 +20,7 @@ export const MainLayout = () => {
             <div 
                 className="fixed inset-0 -z-10 hidden lg:block bg-cover bg-center bg-no-repeat"
                 style={{ 
-                    backgroundImage: `url(${fondoDesktop})`,
+                    backgroundImage: "url('/img/fondo.png')",
                     backgroundSize: "cover",
                     backgroundPosition: "center",
                     backgroundAttachment: "fixed",
